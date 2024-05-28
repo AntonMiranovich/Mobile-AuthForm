281684a0e12e6a40931b78b91d40382a2f22abf5
AntonMiranovich <antonmir992@gmail.com>
2024-05-28 21:56:44 +0300

    add clock

---
+++ monile-auth-form/src/components/Clock/Clock.js
@@ -0,0 +1,12 @@
+import { Button, Image, Text, View, Pressable, TextInput, TouchableOpacity } from 'react-native'
+import styles from './style'
+import { useState } from 'react'
+
+export default function Clock() {
+
+	return (
+		<View style={styles.container}>
+
+		</View>
+	)
+}
