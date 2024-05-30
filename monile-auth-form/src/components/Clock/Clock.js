457650d0cdd0e24475230e3316d22965c96a3df1
AntonMiranovich <antonmir992@gmail.com>
2024-05-30 23:46:59 +0300

    add clock

--- monile-auth-form/src/components/Clock/Clock.js
+++ monile-auth-form/src/components/Clock/Clock.js
@@ -1,25 +1,39 @@
-import { Button, Image, Text, View, Pressable, TextInput, TouchableOpacity,ImageBackground } from 'react-native'
+import { View, ImageBackground } from 'react-native'
 import styles from './style'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import img from '../../../assets/clock.png'
 
 export default function Clock() {
+	const [date, setDate] = useState()
+
+	useEffect(() => {
+		setInterval(() => {
+			setDate(new Date())
+		}, 1000)
+	}, [date])
 
 	return (
 		<View style={styles.container}>
 			<View style={styles.clock}>
 				<ImageBackground source={img} resizeMode="cover" style={styles.image}>
 					<View style={styles.hour}>
-						<View style={styles.hr}></View>
+						<View style={[styles.hr, {
+							transform: [{ rotateZ: `${new Date().getHours() * 30}deg` }],
+						}]}></View>
 					</View>
 					<View style={styles.min}>
-						<View style={styles.mn}></View>
+						<View style={[styles.mn, {
+							transform: [{ rotateZ: `${new Date().getMinutes() * 6}deg` }],
+						}]}></View>
 					</View>
 					<View style={styles.sec}>
-						<View style={styles.sc}></View>
+						<View style={[styles.sc, {
+							transform: [{ rotateZ: `${new Date().getSeconds() * 6}deg` }],
+						}]}></View>
 					</View>
 				</ImageBackground>
-			</View>
-		</View>
+			</View >
+		</View >
 	)
 }
+
--- monile-auth-form/src/components/Clock/style.js
+++ monile-auth-form/src/components/Clock/style.js
@@ -6,10 +6,52 @@ const styles = StyleSheet.create({
         height: 350,
         justifyContent: 'center',
         alignItems: 'center',
+        position: 'relative',
+        zIndex: 1
     },
     image: {
         width: 350,
         height: 350,
+    },
+    hour: {
+        flex: 1,
+        position: 'absolute'
+    },
+    min: {
+        flex: 1,
+        position: 'absolute'
+    },
+    sec: {
+        flex: 1,
+        position: 'absolute'
+    },
+    hr: {
+        width: 6,
+        height: 100,
+        backgroundColor: 'red',
+        left: 172,
+        top: 80,
+        borderRadius: 10,
+        zIndex: 5,
+    },
+    mn: {
+        width: 4,
+        height: 120,
+        backgroundColor: 'black',
+        left: 173,
+        top: 60,
+        borderRadius: 10,
+        zIndex: 6
+    },
+    sc: {
+        width: 2,
+        height: 140,
+        backgroundColor: 'green',
+        left: 173,
+        top: 40,
+        borderRadius: 10,
+        zIndex: 6
     }
 })
+
 export default styles
